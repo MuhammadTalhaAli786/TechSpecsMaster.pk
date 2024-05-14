f8f937e4753cfaf83aeb4455b0b5eff85749564a
HamzaTahir22 <hamzatahirn9005@gmail.com>
2024-05-14 12:23:27 +0500

    Updated Files

--- home/views.py
+++ home/views.py
@@ -70,7 +70,7 @@ def getInTouch(request):
         except Exception as e:
             print("exception", e)
 
-    return redirect("contact")
+    return redirect("home:contact")
 
 
 """
@@ -108,7 +108,7 @@ def dashboard(request):
                        }
                       )
     else:
-        return redirect("home")
+        return redirect("home:home")
         
 def addToWishlist(request,id):
     if request.user.is_authenticated:
