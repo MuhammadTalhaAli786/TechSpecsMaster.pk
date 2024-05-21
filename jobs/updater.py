a7ef110512f1240d7daf617fd8f9d7ddcd2ded75
MALIK ZOHAIB MUSTAFA <56153394+MalikZohaibMustafa@users.noreply.github.com>
2024-05-21 20:58:01 +0500

    Update updater.py

--- jobs/updater.py
+++ jobs/updater.py
@@ -4,5 +4,5 @@ from .startDb import task,task2
 
 def start():
 	scheduler = BackgroundScheduler()
-	scheduler.add_job(task, 'interval', hours=5)
+	scheduler.add_job(task, 'interval', hours=12)
 	scheduler.start()
